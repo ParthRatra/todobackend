cec72b23112e4b642c0e3061afbf5dc4c93be231
Parth Ratra <ratraparth59@gmail.com>
2023-08-18 02:12:36 +0530

    Update deletetodo.js

--- controllers/deletetodo.js
+++ controllers/deletetodo.js
@@ -2,6 +2,9 @@ const Todo =require('../models/Todo')
 
 exports.deleting = async(req,res)=>{
     try{
+        // params id from URL parameter yh vo id hai jo apne app bnti hai mongodb mai
+        // /id bydefauilt bni hai toh uske liye maine params use kiyaa hia
+
         const{id}=req.params
         const todos=await Todo.findByIdAndDelete(
            id 
